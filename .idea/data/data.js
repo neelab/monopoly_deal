450bebdbc7d22a294410ca024551a8b27c354bb1
neelab <neelabalkaran@hotmail.com>
2016-10-11 13:58:31 -0400

    Initial Commit For Array of Cards

---
+++ .idea/data/data.js
@@ -0,0 +1,41 @@
+var _ = require('lodash');
+
+function CardData() {
+    return {
+        cards: [{
+            type: 'Action',
+            name: 'Deal Breaker',
+            colour: 'Purple'
+        }, {
+            type: 'Action',
+            name: 'Deal Breaker',
+            colour: 'Purple'
+        }, {
+            type: 'Action',
+            name: 'Debt Collector',
+            colour: 'Purple'
+        }, {
+            type: 'Property',
+            name: 'Baltic Avenue',
+            colour: 'Brown'
+        }, {
+            type: 'Wildcard',
+            name: '2-Colour Property Wildcard',
+            colour: 'Blue/Green'
+        }, {
+            type: 'Wildcard',
+            name: '10-colour Property Wildcard',
+            colour: 'All'
+        }, {
+            type: 'Rent',
+            name: '2-Colour Rent Card',
+            colour: 'Blue/Green'
+        }, {
+            type: 'Money',
+            name: '10M',
+            colour: 'Gold'
+
+        }]
+    }
+}
+
